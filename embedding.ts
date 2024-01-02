468b2c4714e9c3cb6a555d7ae00d91a6fef8b86c
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-01-02 13:13:19 +0800

    step 4

--- embedding.ts
+++ embedding.ts
@@ -84,6 +84,128 @@ const tiktokenizer = async (files: TextFile[]): Promise<TextFileToken[]> => {
 };
 
 // -----------
+// Step 3
+// -----------
+
+const MAX_TOKENS = 500;
+
+async function splitTextToMany(text: TextFileToken): Promise<TextFileToken[]> {
+  const sentences = text.text
+    .split(". ")
+    .map((sentence) => ({
+      text: sentence,
+      numberTokens: enconding.encode(sentence).length,
+    }))
+    .reduce((acc, sentence) => {
+      // if the sentence is too long, split it by \n
+      if (sentence.numberTokens > MAX_TOKENS) {
+        const sentences = sentence.text.split("\n").map((sentence) => ({
+          text: sentence,
+          numberTokens: enconding.encode(sentence).length,
+        }));
+
+        return [...acc, ...sentences];
+      }
+      return [...acc, sentence];
+    }, [] as { text: string; numberTokens: number }[]);
+
+  const chunks: TextFileToken[] = [];
+
+  let tokensSoFar = 0;
+  let currentChunks: TextFileToken[] = [];
+
+  for (const sentence of sentences) {
+    const numberToken = sentence.numberTokens;
+
+    if (tokensSoFar + numberToken > MAX_TOKENS) {
+      const chunkText = currentChunks.map((c) => c.text).join(". ");
+      chunks.push({
+        filePath: text.filePath,
+        text: currentChunks.map((c) => c.text).join(". "),
+        token: enconding.encode(chunkText),
+      });
+
+      currentChunks = [];
+      tokensSoFar = 0;
+    }
+
+    currentChunks.push({
+      filePath: text.filePath,
+      text: sentence.text,
+      token: new Uint32Array(),
+    });
+
+    tokensSoFar += numberToken;
+  }
+
+  if (currentChunks.length > 0) {
+    const chunkText = currentChunks.map((c) => c.text).join(". ");
+    chunks.push({
+      filePath: text.filePath,
+      text: currentChunks.map((c) => c.text).join(". "),
+      token: enconding.encode(chunkText),
+    });
+  }
+
+  return chunks;
+}
+
+async function splitTexts(texts: TextFileToken[]): Promise<TextFileToken[]> {
+  const shortened: TextFileToken[] = [];
+
+  for (const file of texts) {
+    if (file.token.length > MAX_TOKENS) {
+      const chunks = await splitTextToMany(file);
+      shortened.push(...chunks);
+    } else {
+      shortened.push(file);
+    }
+  }
+
+  return shortened;
+}
+
+// -----------
+// Step 4
+// -----------
+
+type TextFileTokenEmbedding = TextFile & {
+  embedding: number[];
+};
+
+async function processEmbeddings(
+  texts: TextFileToken[]
+): Promise<TextFileTokenEmbedding[]> {
+  const embededs: TextFileTokenEmbedding[] = [];
+  let i = 0;
+
+  for await (const file of texts) {
+    const result = await openai.embeddings.create({
+      model: "text-embedding-ada-002",
+      input: file.text,
+      encoding_format: "float",
+    });
+
+    const embeddings = result.data[0].embedding;
+
+    embededs.push({
+      ...file,
+      embedding: embeddings,
+    });
+
+    i++;
+
+    console.log(
+      "⛏️ Finished embedding: ",
+      file.filePath,
+      `${i}/${texts.length}`
+    );
+  }
+
+  return embededs;
+}
+
+// -----------
 // Main
 // -----------
 
@@ -98,6 +220,16 @@ async function main() {
   const textsTokens = await cache_withFile(
     () => tiktokenizer(texts),
     "processed/textsTokens.json"
+  );
+
+  const textsTokensShortened = await cache_withFile(
+    () => splitTexts(textsTokens),
+    "processed/textsTokensShortened.json"
+  );
+
+  const textsTokensEmbeddings = await cache_withFile(
+    () => processEmbeddings(textsTokensShortened),
+    "processed/textsTokensEmbeddings.json"
   );
 }
 
